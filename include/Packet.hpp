83e14041767c2ec003e8397570940da7dcb605b9
Jerel Harris <jereljr@gmail.com>
2023-04-26 22:56:46 -0400

    Switched to netinet headers

--- include/Packet.hpp
+++ include/Packet.hpp
@@ -8,14 +8,14 @@
 #include <ifaddrs.h>
 #include <inttypes.h>
 #include <iostream>
-#include <linux/icmp.h>
 #include <linux/igmp.h>
-#include <linux/ip.h>
 #include <linux/tcp.h>
 #include <linux/udp.h>
 #include <memory>
 #include <netinet/if_ether.h>
 #include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
 #include <optional>
 #include <string.h>
 // #include <sys/capability.h>
@@ -25,8 +25,18 @@
 
 typedef struct ethhdr _ethhdr;
 typedef struct iphdr _iphdr;
+typedef struct icmphdr _icmphdr;
 typedef struct tcphdr _tcphdr;
 typedef struct udphdr _udphdr;
+
+enum protocol_filter
+{
+	NONE = -1,
+	ICMP = 1,
+	IGMP,
+	TCP = 6,
+	UDP = 17
+};
 
 struct SnifferOptions
 {
@@ -34,14 +44,7 @@ struct SnifferOptions
 	std::optional<std::string> dest_IP;
 	std::optional<uint32_t> n_packets;
 	std::optional<uint16_t> port;
-	enum protocol_filter
-	{
-		NONE = -1,
-		ICMP = 1,
-		IGMP,
-		TCP = 6,
-		UDP = 17
-	} p_filter = NONE;
+	protocol_filter p_filter;
 };
 
 void print_interfaces();
@@ -51,6 +54,8 @@ SnifferOptions parse_args(int argc, char *const *argv);
 void print_ethhdr(const _ethhdr &eth);
 
 void print_iphdr(const _iphdr &ip);
+
+void print_icmphdr(const _icmphdr &icmp);
 
 void print_tcphdr(const _tcphdr &tcp);
 
